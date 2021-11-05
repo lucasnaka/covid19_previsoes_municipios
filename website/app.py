4dc42e059f17450fbb7373663ffda6a18f86e0b2
lu_naka <lunetonaka@gmail.com>
2021-11-05 13:44:54 -0300

    stacked bar 100 fixed, mari branch merged

--- website/app.py
+++ website/app.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import streamlit as st 
+import streamlit as st
 from PIL import Image
 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import matplotlib
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import seaborn as sns
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import datetime as dt
 
@@ -23,15 +24,10 @@ csv_file = pd.read_csv('web_data/est_cidade.csv', sep=';', encoding='latin-1')
 states = list(csv_file['UF'].drop_duplicates().sort_values())
 
 df_casos = pd.read_parquet('../data/app/covid_saude_obito_municipio.parquet')
+df_casos_reg = pd.read_parquet('../data/app/covid_saude_obito_regiao.parquet')
+
 df_casos['data'] = pd.to_datetime(df_casos['data'])
-
-df_boletim_direitos = pd.read_csv('../data/raw/boletim_covid/boletim_direitos_covid.csv', encoding='latin-1')
-df_boletim_direitos['data'] = pd.to_datetime(df_boletim_direitos['data'], format='%d/%m/%Y')
-
-df_boletim_direitos['noticia'] = df_boletim_direitos['noticia'].str.wrap(30)
-df_boletim_direitos['noticia'] = df_boletim_direitos['noticia'].apply(lambda x: x.replace('\n', '<br>'))
-
-df_casos = df_casos.merge(df_boletim_direitos, how='left', on='data')
+df_casos_reg['data'] = pd.to_datetime(df_casos_reg['data'])
 
 def SetNewsSize(x):
     if not pd.isnull(x):
@@ -39,8 +35,10 @@ def SetNewsSize(x):
     else:
         return 0
 
+
 def find_cities(selected_states):
-    return list(csv_file[csv_file['UF']==selected_states]['Município'].sort_values())
+    return list(csv_file[csv_file['UF'] == selected_states]['Município'].sort_values())
+
 
 def filter_state_city():
     choose_state = st.sidebar.checkbox('Filtrar por estado')
@@ -53,80 +51,105 @@ def filter_state_city():
         choose_city = st.sidebar.checkbox('Filtrar por cidade')
     if choose_city:
         selected_city = st.sidebar.selectbox('Cidade', find_cities(selected_state))
-    
+
     return selected_state, selected_city
+
 
 def filter_date(df):
     choose_date = st.sidebar.checkbox('Filtrar por data')
     min_date = df['data'].min()
     max_date = df['data'].max()
-    date_range = (dt.date(min_date.year, min_date.month, min_date.day), dt.date(max_date.year, max_date.month, max_date.day))
-    selected_date = None    
+    date_range = (
+        dt.date(min_date.year, min_date.month, min_date.day), dt.date(max_date.year, max_date.month, max_date.day))
+    selected_date = None
 
     if choose_date:
         selected_date = st.sidebar.slider('Data', value=date_range, format="DD/MM/YYYY")
 
     return selected_date
 
-def filters(df_casos):
+
+def filters(df_casos, df_casos_reg):
     selected_filters = dict()
     selected_data = st.sidebar.selectbox('Dados disponíveis', ('Casos Confirmados', 'Óbitos', 'Vacinação'))
     selected_state, selected_city = filter_state_city()
-    
-    selected_filters['state'] =  selected_state
-    selected_filters['city']  = selected_city
-    
+
+    selected_filters['state'] = selected_state
+    selected_filters['city'] = selected_city
+
     if selected_filters['city']:
-        df_filtered = df_casos.loc[(df_casos['municipio']==selected_filters['city'])]
+        df_filtered = df_casos.loc[(df_casos['municipio'] == selected_filters['city'])]
     else:
-        df_filtered = df_casos.loc[(df_casos['municipio']=='São Paulo')]
-        
+        df_filtered = df_casos.loc[(df_casos['municipio'] == 'São Paulo')]
+
     selected_date_range = filter_date(df_filtered)
 
     selected_filters['database'] = selected_data
     selected_filters['date'] = selected_date_range
-    
+
     if selected_filters['date']:
-        df_filtered = df_filtered.loc[(df_filtered['data'].dt.date>=selected_filters['date'][0]) & (df_filtered['data'].dt.date<=selected_filters['date'][1])]
-    
-    return selected_filters, df_filtered
+        df_filtered = df_filtered.loc[(df_filtered['data'].dt.date >= selected_filters['date'][0])
+                                      & (df_filtered['data'].dt.date <= selected_filters['date'][1])]
+        df_casos_reg = df_casos_reg.loc[(df_casos_reg['data'].dt.date >= selected_filters['date'][0])
+                                           & (df_casos_reg['data'].dt.date <= selected_filters['date'][1])]
+
+    return selected_filters, df_filtered, df_casos_reg
+
 
 def home():
     st.title('Início')
 
+
 def predictive_models():
     st.title('Modelos preditivos')
 
-    selected_filters, df_filtered = filters(df_casos)
+    selected_filters, df_filtered, df_filtered_reg = filters(df_casos, df_casos_reg)
     if selected_filters:
-        st.info(f"{selected_filters}") 
+        st.info(f"{selected_filters}")
 
 
 def descriptive_models():
     st.title('Modelos descritivos')
 
-    selected_filters, df_filtered = filters(df_casos)
+    selected_filters, df_filtered, df_filtered_reg = filters(df_casos, df_casos_reg)
     if selected_filters:
         st.info(f"{selected_filters}")
-    
-#     st.title('Número de óbitos por dia')
+
+    #     st.title('Número de óbitos por dia')
     fig = go.Figure()
 
-    fig.add_trace(go.Scatter(x=df_filtered["data"], 
-                             y=df_filtered["obitosNovos"], 
-                             text=df_filtered['noticia'],
+    fig.add_trace(go.Scatter(x=df_filtered["data"],
+                             y=df_filtered["obitosNovos"],
+                             text=df_filtered['event'],
                              hoverinfo='text',
                              mode='lines+markers',
-                             marker = dict(size=list(map(SetNewsSize, df_filtered['noticia'])),
-                                           color=['orange']*df_filtered.shape[0])
+                             marker=dict(size=list(map(SetNewsSize, df_filtered['event'])),
+                                         color=['orange'] * df_filtered.shape[0])
                              )
-                 )
-#                   hover_data=['noticia'], 
-#                   title='Número de óbitos por dia')
-    
+                  )
+    #                   hover_data=['noticia'],
+    #                   title='Número de óbitos por dia')
+
     st.write(fig)
+
+    fig = px.bar(df_filtered_reg,
+                 x=df_filtered_reg['data'],
+                 y=df_filtered_reg['percentage_deaths'],
+                 color='regiao',
+                 text=df_filtered_reg['percentage_deaths'].apply(lambda x: '{0:1.2f}%'.format(x)))
+    fig.update_layout(title="Stacked bar with '%' for Regions",
+                      title_font_color="black",
+                      yaxis_title="% Casos por Regiao",
+                      font=dict(
+                          family="arial",
+                          size=18),
+                      template="plotly_white")
+
+    st.write(fig)
+
+
 #     st.line_chart(df_filtered[['data', 'obitosNovos']].set_index('data'))
-        
+
 def about():
     st.title('Sobre')
 
@@ -156,6 +179,7 @@ def about():
     #### PROBABILIDADE E ESTATÍSTICA 
     #### MECAI - 2021
     """)
+
 
 # In[ ]:
 if selected_page == 'Início':
