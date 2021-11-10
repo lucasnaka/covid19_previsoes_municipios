c188bf11fbceb580a3e519acf5c56e8b3c2666c0
lu_naka <lunetonaka@gmail.com>
2021-11-10 18:37:56 -0300

    boletim covid associado ao grafico de obitos diarios

--- website/app.py
+++ website/app.py
@@ -15,206 +15,196 @@ import numpy as np
 import datetime as dt
 
 
-def main():
-    st.set_page_config(
-        layout="wide",
-        initial_sidebar_state="expanded")
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded")
 
-    matplotlib.use("Agg")
-    st.title("Previsão de Obitos COVID-19 - ICMC/MECAI - USP")
+matplotlib.use("Agg")
+st.title("Previsão de Obitos COVID-19 - ICMC/MECAI - USP")
 
-    st.sidebar.title('Menu')
-    pages = ('Início', 'Modelos preditivos', 'Modelos descritivos', 'Sobre')
-    selected_page = st.sidebar.radio('Paginas', pages)
+st.sidebar.title('Menu')
+pages = ('Início', 'Modelos preditivos', 'Modelos descritivos', 'Sobre')
+selected_page = st.sidebar.radio('Paginas', pages)
 
-    csv_file = pd.read_csv('website/web_data/est_cidade.csv', sep=';', encoding='latin-1')
-    states = list(csv_file['UF'].drop_duplicates().sort_values())
+df_casos = pd.read_parquet('../data/app/covid_saude_obito_municipio.parquet')
+df_casos_reg = pd.read_parquet('../data/app/covid_saude_obito_regiao.parquet')
+csv_file = pd.read_parquet('../data/app/est_cidade.parquet')
+states = list(csv_file['estado'].drop_duplicates().sort_values())
 
-    df_casos = pd.read_parquet('data/app/covid_saude_obito_municipio.parquet')
-    df_casos_reg = pd.read_parquet('data/app/covid_saude_obito_regiao.parquet')
-    # csv_file = pd.read_csv('web_data/est_cidade.csv', sep=';', encoding='latin-1')
-    # states = list(csv_file['UF'].drop_duplicates().sort_values())
+df_casos['data'] = pd.to_datetime(df_casos['data'])
+df_casos_reg['data'] = pd.to_datetime(df_casos_reg['data'])
 
-    # df_casos = pd.read_parquet('../data/app/covid_saude_obito_municipio.parquet')
-    # df_casos_reg = pd.read_parquet('../data/app/covid_saude_obito_regiao.parquet')
+def SetNewsSize(x):
+    if not pd.isnull(x):
+        return 7.5
+    else:
+        return 0
 
-    df_casos['data'] = pd.to_datetime(df_casos['data'])
-    df_casos_reg['data'] = pd.to_datetime(df_casos_reg['data'])
+def find_cities(selected_states):
+    return list(csv_file[csv_file['UF'] == selected_states]['Município'].sort_values())
 
-    def SetNewsSize(x):
-        if not pd.isnull(x):
-            return 7.5
-        else:
-            return 0
+def filter_state_city():
+    choose_state = st.sidebar.checkbox('Filtrar por estado')
+    choose_city = None
+    selected_state = None
+    selected_city = None
 
-    def find_cities(selected_states):
-        return list(csv_file[csv_file['UF'] == selected_states]['Município'].sort_values())
+    if choose_state:
+        selected_state = st.sidebar.selectbox('Estado', states)
+        choose_city = st.sidebar.checkbox('Filtrar por cidade')
+    if choose_city:
+        selected_city = st.sidebar.selectbox('Cidade', find_cities(selected_state))
 
-    def filter_state_city():
-        choose_state = st.sidebar.checkbox('Filtrar por estado')
-        choose_city = None
-        selected_state = None
-        selected_city = None
+    return selected_state, selected_city
 
-        if choose_state:
-            selected_state = st.sidebar.selectbox('Estado', states)
-            choose_city = st.sidebar.checkbox('Filtrar por cidade')
-        if choose_city:
-            selected_city = st.sidebar.selectbox('Cidade', find_cities(selected_state))
+def filter_date(df):
+    choose_date = st.sidebar.checkbox('Filtrar por data')
+    min_date = df['data'].min()
+    max_date = df['data'].max()
+    date_range = (
+        dt.date(min_date.year, min_date.month, min_date.day), dt.date(max_date.year, max_date.month, max_date.day))
+    selected_date = None
 
-        return selected_state, selected_city
+    if choose_date:
+        selected_date = st.sidebar.slider('Data', value=date_range, format="DD/MM/YYYY")
 
-    def filter_date(df):
-        choose_date = st.sidebar.checkbox('Filtrar por data')
-        min_date = df['data'].min()
-        max_date = df['data'].max()
-        date_range = (
-            dt.date(min_date.year, min_date.month, min_date.day), dt.date(max_date.year, max_date.month, max_date.day))
-        selected_date = None
+    return selected_date
 
-        if choose_date:
-            selected_date = st.sidebar.slider('Data', value=date_range, format="DD/MM/YYYY")
+def filters(df_casos, df_casos_reg):
+    selected_filters = dict()
+    selected_data = st.sidebar.selectbox('Dados disponíveis', ('Casos Confirmados', 'Óbitos', 'Vacinação'))
+    selected_state, selected_city = filter_state_city()
 
-        return selected_date
+    selected_filters['state'] = selected_state
+    selected_filters['city'] = selected_city
 
-    def filters(df_casos, df_casos_reg):
-        selected_filters = dict()
-        selected_data = st.sidebar.selectbox('Dados disponíveis', ('Casos Confirmados', 'Óbitos', 'Vacinação'))
-        selected_state, selected_city = filter_state_city()
+    if selected_filters['city']:
+        df_filtered = df_casos.loc[(df_casos['municipio'] == selected_filters['city'])]
+    else:
+        df_filtered = df_casos.loc[(df_casos['municipio'] == 'São Paulo')]
 
-        selected_filters['state'] = selected_state
-        selected_filters['city'] = selected_city
+    selected_date_range = filter_date(df_filtered)
 
-        if selected_filters['city']:
-            df_filtered = df_casos.loc[(df_casos['municipio'] == selected_filters['city'])]
-        else:
-            df_filtered = df_casos.loc[(df_casos['municipio'] == 'São Paulo')]
+    selected_filters['database'] = selected_data
+    selected_filters['date'] = selected_date_range
 
-        selected_date_range = filter_date(df_filtered)
+    if selected_filters['date']:
+        df_filtered = df_filtered.loc[(df_filtered['data'].dt.date >= selected_filters['date'][0])
+                                      & (df_filtered['data'].dt.date <= selected_filters['date'][1])]
+        df_casos_reg = df_casos_reg.loc[(df_casos_reg['data'].dt.date >= selected_filters['date'][0])
+                                        & (df_casos_reg['data'].dt.date <= selected_filters['date'][1])]
 
-        selected_filters['database'] = selected_data
-        selected_filters['date'] = selected_date_range
+    return selected_filters, df_filtered, df_casos_reg
 
-        if selected_filters['date']:
-            df_filtered = df_filtered.loc[(df_filtered['data'].dt.date >= selected_filters['date'][0])
-                                          & (df_filtered['data'].dt.date <= selected_filters['date'][1])]
-            df_casos_reg = df_casos_reg.loc[(df_casos_reg['data'].dt.date >= selected_filters['date'][0])
-                                            & (df_casos_reg['data'].dt.date <= selected_filters['date'][1])]
+def home():
+    st.title('Início')
 
-        return selected_filters, df_filtered, df_casos_reg
+def predictive_models():
+    st.title('Modelos preditivos')
 
-    def home():
-        st.title('Início')
+    selected_filters, df_filtered, df_filtered_reg = filters(df_casos, df_casos_reg)
+    if selected_filters:
+        st.info(f"{selected_filters}")
 
-    def predictive_models():
-        st.title('Modelos preditivos')
+def descriptive_models():
+    st.header('Modelos descritivos')
 
-        selected_filters, df_filtered, df_filtered_reg = filters(df_casos, df_casos_reg)
-        if selected_filters:
-            st.info(f"{selected_filters}")
+    col1, col2, col3 = st.beta_columns((13.0, 2.0, 1.5))
 
-    def descriptive_models():
-        st.header('Modelos descritivos')
+    selected_filters, df_filtered, df_filtered_reg = filters(df_casos, df_casos_reg)
+    if selected_filters:
+        st.info(f"{selected_filters}")
 
-        col1, col2, col3 = st.beta_columns((13.0, 2.0, 1.5))
+    #     st.title('Número de óbitos por dia')
+    fig = go.Figure()
 
-        selected_filters, df_filtered, df_filtered_reg = filters(df_casos, df_casos_reg)
-        if selected_filters:
-            st.info(f"{selected_filters}")
+    fig.add_trace(go.Scatter(x=df_filtered["data"],
+                             y=df_filtered["obitosNovos"],
+                             text=df_filtered['texto'],
+                             hoverinfo='text',
+                             mode='lines+markers',
+                             marker=dict(size=list(map(SetNewsSize, df_filtered['texto'])),
+                                         color=['orange'] * df_filtered.shape[0]),
+                             )
+                  )
 
-        #     st.title('Número de óbitos por dia')
-        fig = go.Figure()
+    fig.update_layout(title="Óbitos diários",
+                      title_font_color="black",
+                      yaxis_title="Número de óbitos",
+                      font=dict(
+                          family="arial",
+                          size=14),
+                      template="plotly_white",
+                      margin=dict(b=0))
 
-        fig.add_trace(go.Scatter(x=df_filtered["data"],
-                                 y=df_filtered["obitosNovos"],
-                                 text=df_filtered['event'],
-                                 hoverinfo='text',
-                                 mode='lines+markers',
-                                 marker=dict(size=list(map(SetNewsSize, df_filtered['event'])),
-                                             color=['orange'] * df_filtered.shape[0])
-                                 ),
-                      title='Número de óbitos por dia')
+    col1.plotly_chart(fig, use_container_width=True)
 
-        fig.update_layout(title="Óbitos diários",
-                          title_font_color="black",
-                          yaxis_title="Número de óbitos",
-                          font=dict(
-                              family="arial",
-                              size=14),
-                          template="plotly_white",
-                          margin=dict(b=0))
+    fig = px.bar(df_filtered_reg,
+                 x=df_filtered_reg['data'],
+                 y=df_filtered_reg['percentage_deaths'],
+                 color='regiao',
+                 labels={
+                     "regiao": "Região",
+                 },)
+                 # text=df_filtered_reg['percentage_deaths'].apply(lambda x: '{0:1.2f}%'.format(x)))
+    fig.update_layout(yaxis_title="% Óbitos semanais por região",
+                      font=dict(
+                          family="arial",
+                          size=14),
+                      template="plotly_white",
+                      margin=dict(t=0))
 
-        col1.plotly_chart(fig, use_container_width=True)
+    col1.plotly_chart(fig, use_container_width=True)
 
-        fig = px.bar(df_filtered_reg,
-                     x=df_filtered_reg['data'],
-                     y=df_filtered_reg['percentage_deaths'],
-                     color='regiao',
-                     labels={
-                         "regiao": "Região",
-                     },
-                     text=df_filtered_reg['percentage_deaths'].apply(lambda x: '{0:1.2f}%'.format(x)))
-        fig.update_layout(yaxis_title="% Óbitos semanais por região",
-                          font=dict(
-                              family="arial",
-                              size=14),
-                          template="plotly_white",
-                          margin=dict(t=0))
+    mean_ob = np.mean(df_filtered["obitosNovos"])
+    new_title = f'<p style="background: #76BCFB;width: 100px; height: 100px; font-family:arial; color:Black; font-size: 18px;">Média Obitos BR: {mean_ob: .2f}</p>'
+    col2.markdown(new_title, unsafe_allow_html=True)
 
-        col1.plotly_chart(fig, use_container_width=True)
+    new_title = f'<p style="background: #76BCFB; ;width: 100px; height: 100px;font-family:arial; color:Black; font-size: 18px;">Média Obitos BR: {mean_ob: .2f}</p>'
+    col3.markdown(new_title, unsafe_allow_html=True)
 
-        mean_ob = np.mean(df_filtered["obitosNovos"])
-        new_title = f'<p style="background: #76BCFB;width: 100px; height: 100px; font-family:arial; color:Black; font-size: 18px;">Média Obitos BR: {mean_ob: .2f}</p>'
-        col2.markdown(new_title, unsafe_allow_html=True)
+    new_title = f'<p style="background: #76BCFB;width: 100px; height: 100px; font-family:arial; color:Black; font-size: 18px;">Média Obitos BR: {mean_ob: .2f}</p>'
+    col2.markdown(new_title, unsafe_allow_html=True)
 
-        new_title = f'<p style="background: #76BCFB; ;width: 100px; height: 100px;font-family:arial; color:Black; font-size: 18px;">Média Obitos BR: {mean_ob: .2f}</p>'
-        col3.markdown(new_title, unsafe_allow_html=True)
+    new_title = f'<p style="background: #76BCFB; ;width: 100px; height: 100px;font-family:arial; color:Black; font-size: 18px;">Média Obitos BR: {mean_ob: .2f}</p>'
+    col3.markdown(new_title, unsafe_allow_html=True)
 
-        new_title = f'<p style="background: #76BCFB;width: 100px; height: 100px; font-family:arial; color:Black; font-size: 18px;">Média Obitos BR: {mean_ob: .2f}</p>'
-        col2.markdown(new_title, unsafe_allow_html=True)
+def about():
+    st.title('Sobre')
 
-        new_title = f'<p style="background: #76BCFB; ;width: 100px; height: 100px;font-family:arial; color:Black; font-size: 18px;">Média Obitos BR: {mean_ob: .2f}</p>'
-        col3.markdown(new_title, unsafe_allow_html=True)
+    st.markdown("""
 
-    def about():
-        st.title('Sobre')
+    ### O Projeto
+    Plataforma Web para disponibilizar publicamente a previsão de casos 
+    de óbito e vacinação relacionados à Covid-19 em nível municipal.
 
-        st.markdown("""
+    ### A Gerência
+    | Membro | Função |
+    | ------ | ------ |
+    | Francisco Louzada Neto | CEO |
+    | Loriz Sallum | Diretora |
+    | Oilson Gonzatto | Diretor |
+
+    ### Os membros da equipe
+    | Aluno | Função |
+    | ------ | ------ |
+    | Bernardo | Estatístico |
+    | Bruno Braziel | Programador |
+    | Francisco Pigato | Coordenador |
+    | Lucas Nakadaira | Programador |
+    | Mariana Spanol | Estatística |
     
-        ### O Projeto
-        Plataforma Web para disponibilizar publicamente a previsão de casos 
-        de óbito e vacinação relacionados à Covid-19 em nível municipal.
 
-        ### A Gerência
-        | Membro | Função |
-        | ------ | ------ |
-        | Francisco Louzada Neto | CEO |
-        | Loriz Sallum | Diretora |
-        | Oilson Gonzatto | Diretor |
-
-        ### Os membros da equipe
-        | Aluno | Função |
-        | ------ | ------ |
-        | Bernardo | Estatístico |
-        | Bruno Braziel | Programador |
-        | Francisco Pigato | Coordenador |
-        | Lucas Nakadaira | Programador |
-        | Mariana Spanol | Estatística |
-        
-
-        #### PROBABILIDADE E ESTATÍSTICA 
-        #### MECAI - 2021
-        """)
-
-    # In[ ]:
-    if selected_page == 'Início':
-        home()
-    elif selected_page == 'Modelos preditivos':
-        predictive_models()
-    elif selected_page == 'Modelos descritivos':
-        descriptive_models()
-    elif selected_page == 'Sobre':
-        about()
+    #### PROBABILIDADE E ESTATÍSTICA 
+    #### MECAI - 2021
+    """)
 
 
-main()
+if selected_page == 'Início':
+    home()
+elif selected_page == 'Modelos preditivos':
+    predictive_models()
+elif selected_page == 'Modelos descritivos':
+    descriptive_models()
+elif selected_page == 'Sobre':
+    about()
